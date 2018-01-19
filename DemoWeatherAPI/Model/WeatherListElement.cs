488ebdef6b7b6cb6add6c951bed1a58b18a87dc3
DMagyar <dmagyar@nware.loc>
2018-01-19 11:00:11 +0100

    Weater -> Weather again -.-

--- DemoWeatherAPI/Model/WeatherListElement.cs
+++ DemoWeatherAPI/Model/WeatherListElement.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DemoWeatherAPI.Model
 {
-    public class WheaterListElement
+    public class WeatherListElement
     {
         /// <summary>
         /// Elsődleges paraméterek
